d4a124475d61fa0113c8310fb7021bb852c13749
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-08 11:58:29 -0600

    Add lots of stuff: form, card, and idea component functions

--- src/component/Idea/Idea.js
+++ src/component/Idea/Idea.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import Card from './Card/Card';
+import Card from '../Card/Card';
 import './Idea.css';
 
-const Idea = ({ideas , deleteIdeas}) => {
+const Idea = ({ideas, deleteIdea}) => {
 
   // A method that maps over props to create individual instances 
   // of each card
+  const ideaCards = ideas.map(idea => {
+    return (
+      <Card 
+        id={idea.id}
+        title={idea.title}
+        description={idea.description}
+        deleteIdea={deleteIdea}
+
+      />
+    )
+  })
 
 
   return (
     <div className="idea-container"> 
-
+      {ideaCards}
     </div>
 
   )
