27379417eb8ae287d2cd3a974f8ef485e19e1210
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-07-08 11:14:31 -0600

    Add file structure to Card and Idea components

---
+++ src/component/Idea/Idea.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './Card/Card';
+import './Idea.css';
+
+const Idea = ({ideas , deleteIdeas}) => {
+
+  // A method that maps over props to create individual instances 
+  // of each card
+
+
+  return (
+    <div className="idea-container"> 
+
+    </div>
+
+  )
+
+}
+
+export default Idea;
